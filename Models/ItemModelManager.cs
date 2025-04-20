1529ed7847c4e1733117253eb885d1afaf84711d
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-04-20 02:05:51 -0600

    Adds Add Item Tab

---
+++ Models/ItemModelManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Renting.Models
+{
+    public class ItemModelManager
+    {
+        public static string JSONPATH = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\Resources\Res\items_data.json");
+        public static List<ItemModel> items = new List<ItemModel>();
+        public static List<string> categories = new List<string>();
+        public static string errorMessage = "";
+
+        public static void AddItem()
+        {
+            try
+            {
+                ItemModelManager.GetItems();
+                ItemModelManager.GetCategories();
+                string content = JsonSerializer.Serialize(items);
+                File.WriteAllText(JSONPATH, content);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+        public static void GetItems()
+        {
+
+            try
+            {
+                if (File.Exists(JSONPATH) == true)
+                {
+                    var content = File.ReadAllText(JSONPATH);
+
+                    if (content != null)
+                    {
+                        var filecontent = JsonSerializer.Deserialize<List<ItemModel>>(content);
+                        items.AddRange(filecontent);
+                    }
+
+                }
+                else
+                {
+                    File.Create(JSONPATH).Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+
+        }
+        public static void GetCategories()
+        {
+            try
+            {
+                ItemModelManager.GetItems();
+                foreach (ItemModel item in items)
+                {
+                    categories.Add(item.Category);
+                }
+                errorMessage = "";
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
+}
