70eb274d18e97006f5874ce3ed8aabb669b040c7
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-04-20 15:16:35 -0600

    Adds remove tab

--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -9,19 +9,22 @@ namespace Renting.Models
 {
     public class ItemModel
     {
+        public string Id { get; set; }
         public string Title { get; set; }
         public double Cost { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public string ImageBase64 { get; set; }
-
+        public string Status { get; set; }
         public ItemModel() { }
-        public ItemModel(string title, double cost, string category, string description)
+        public ItemModel(string id, string title, double cost, string category, string description, string status)
         {
+            Id = id;
             Title = title;
             Cost = cost;
             Category = category;
             Description = description;
+            Status = status;
         }
 
     }
